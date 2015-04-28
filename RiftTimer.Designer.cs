0b158ad6dac450b5116319ff61ff2b93e795f219
zajrik <zajrik@gmail.com>
2015-04-27 21:01:11 -0500

    Initial commit

---
+++ RiftTimer.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace rift_timer
+{
+    partial class RiftTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.internalClock = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pauseIndicator = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.finishIndicator = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.classesDropDown = new System.Windows.Forms.ComboBox();
+            this.difficultyDropDown = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Finish";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Finish_Click);
+            // 
+            // internalClock
+            // 
+            this.internalClock.Tick += new System.EventHandler(this.InternalClock_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(302, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // pauseIndicator
+            // 
+            this.pauseIndicator.AutoSize = true;
+            this.pauseIndicator.BackColor = System.Drawing.Color.Transparent;
+            this.pauseIndicator.Location = new System.Drawing.Point(16, 2);
+            this.pauseIndicator.Name = "pauseIndicator";
+            this.pauseIndicator.Size = new System.Drawing.Size(77, 13);
+            this.pauseIndicator.TabIndex = 5;
+            this.pauseIndicator.Text = "pauseIndicator";
+            // 
+            // logBox
+            // 
+            this.logBox.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logBox.FormattingEnabled = true;
+            this.logBox.ItemHeight = 11;
+            this.logBox.Location = new System.Drawing.Point(4, 68);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(398, 147);
+            this.logBox.TabIndex = 6;
+            this.logBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.logBox_DrawItem);
+            this.logBox.MouseHover += new System.EventHandler(this.logBox_MouseHover);
+            // 
+            // finishIndicator
+            // 
+            this.finishIndicator.AutoSize = true;
+            this.finishIndicator.Location = new System.Drawing.Point(161, 2);
+            this.finishIndicator.Name = "finishIndicator";
+            this.finishIndicator.Size = new System.Drawing.Size(72, 13);
+            this.finishIndicator.TabIndex = 7;
+            this.finishIndicator.Text = "finishIndicator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(47, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "sec";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ms";
+            // 
+            // classesDropDown
+            // 
+            this.classesDropDown.AllowDrop = true;
+            this.classesDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.classesDropDown.FormattingEnabled = true;
+            this.classesDropDown.Location = new System.Drawing.Point(4, 219);
+            this.classesDropDown.Name = "classesDropDown";
+            this.classesDropDown.Size = new System.Drawing.Size(189, 21);
+            this.classesDropDown.TabIndex = 11;
+            this.classesDropDown.SelectedIndexChanged += new System.EventHandler(this.DropDown_SelectedIndexChanged);
+            // 
+            // difficultyDropDown
+            // 
+            this.difficultyDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.difficultyDropDown.FormattingEnabled = true;
+            this.difficultyDropDown.Location = new System.Drawing.Point(204, 219);
+            this.difficultyDropDown.Name = "difficultyDropDown";
+            this.difficultyDropDown.Size = new System.Drawing.Size(198, 21);
+            this.difficultyDropDown.TabIndex = 12;
+            this.difficultyDropDown.SelectedIndexChanged += new System.EventHandler(this.DropDown_SelectedIndexChanged);
+            // 
+            // RiftTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(406, 244);
+            this.Controls.Add(this.difficultyDropDown);
+            this.Controls.Add(this.classesDropDown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.finishIndicator);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.pauseIndicator);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "RiftTimer";
+            this.Text = "Rift Timer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RiftTimer_FormClosed);
+            this.Load += new System.EventHandler(this.RiftTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer internalClock;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label pauseIndicator;
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.Label finishIndicator;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox classesDropDown;
+        private System.Windows.Forms.ComboBox difficultyDropDown;
+    }
+}
+
---
+++ RiftTimer.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace rift_timer
+{
+    public partial class RiftTimer : Form
+    {
+        public RiftTimer()
+        {
+            InitializeComponent();
+        }
+
+        private Stopwatch time = new Stopwatch();
+        private int tick = 0;
+
+        private List<string> riftsList = new List<string>();
+        private String entryStr;
+        private int entryNum = 0;
+
+        private Boolean running = false;
+        private Boolean paused = false;
+        private Boolean finished = false;
+
+        private List<string> classesList = new List<string>
+        {
+            "Barbarian",
+            "Crusader",
+            "Demon Hunter",
+            "Monk",
+            "Witch Doctor",
+            "Wizard"
+        };
+        private List<string> difficultyList = new List<string>
+        {
+            "Normal",
+            "Hard",
+            "Expert",
+            "Master",
+            "Torment I",
+            "Torment II",
+            "Torment III",
+            "Torment IV",
+            "Torment V",
+            "Torment VI"
+        };
+
+        private int playerClass = rift_timer.Properties.Settings.Default.playerClass;
+        private int difficulty = rift_timer.Properties.Settings.Default.difficulty;
+
+        private void RiftTimer_Load(object sender, EventArgs e)
+        {
+            CheckTime();
+
+            pauseIndicator.Text = "paused";
+            pauseIndicator.Hide();
+            finishIndicator.Text = "finished";
+            finishIndicator.Hide();
+
+            logBox.DataSource = riftsList;
+            logBox.DrawMode = DrawMode.OwnerDrawFixed;
+
+            classesDropDown.DataSource = classesList;
+            classesDropDown.SelectedIndex = playerClass;
+            difficultyDropDown.DataSource = difficultyList;
+            difficultyDropDown.SelectedIndex = difficulty;
+
+            internalClock.Interval = 10;
+            internalClock.Start();
+        }
+
+        //// Button actions
+        // Start button
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if ((!running && !finished) || paused)
+            {
+                time.Start();
+                running = true;
+                finished = false;
+                SetPauseState(false);
+            }
+            else if (finished)
+            {
+                time.Restart();
+                running = true;
+                finished = false;
+                SetPauseState(false);
+            }
+        }
+
+        // Finish button
+        private void Finish_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                time.Stop();
+                running = false;
+                SetPauseState(false);
+                finished = true;
+                CheckTime();
+
+                // Add new log to list
+                entryNum++;
+                entryStr = String.Format
+                    (
+                        "{0}{1,-5}{2,-3}{3, -10}{4,-3}{5,-14}{6,-3}{7}",
+                        "Rift #", entryNum.ToString("D3"), "|",
+                        time.Elapsed.ToString("mm\\:ss\\:ff"), "|",
+                        classesList[classesDropDown.SelectedIndex], "|",
+                        difficultyList[difficultyDropDown.SelectedIndex]
+                    );
+                riftsList.Add(entryStr);
+                BindLogData();
+            }
+        }
+
+        // Pause button
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                time.Stop();
+                CheckTime();
+                SetPauseState(true);
+                BindLogData();
+            }
+        }
+
+        // Reset button
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            time.Stop();
+            time.Reset();
+            SetPauseState(false);
+            running = false;
+            finished = false;
+            CheckTime();
+        }
+        //// End button actions
+
+        // Get and display current time of stopwatch
+        private void CheckTime()
+        {
+            label1.Text = time.Elapsed.ToString("mm\\:ss\\:ff");
+        }
+
+        // Set pause state, clear pause indicator if unpausing
+        private void SetPauseState(Boolean state)
+        {
+            if (state)
+            {
+                paused = true;
+            }
+            else
+            {
+                paused = false;
+                pauseIndicator.Hide();
+            }
+        }
+
+        // Internal clock
+        private void InternalClock_Tick(object sender, EventArgs e)
+        {
+            CheckTime();
+            // Flash pause indicator on 10 tick intervals
+            if (paused)
+            {
+                if (tick <= 75)
+                {
+                    pauseIndicator.Show();
+                }
+                else if (tick <= 150)
+                {
+                    pauseIndicator.Hide();
+                }
+                else if (tick > 150)
+                {
+                    tick = 0;
+                }
+                tick++;
+            }
+            // Show/hide finish indicator
+            if (finished)
+            {
+                finishIndicator.Show();
+            }
+            else
+            {
+                finishIndicator.Hide();
+            }
+        }
+
+        // Bind selected class and difficulty to preferences
+        private void BindSelectionData(int selectedClass, int selectedDifficulty)
+        {
+            rift_timer.Properties.Settings.Default.playerClass = selectedClass;
+            rift_timer.Properties.Settings.Default.difficulty = selectedDifficulty;
+            rift_timer.Properties.Settings.Default.Save();
+        }
+
+        // Bind selection data when either of the class/difficulty dropdowns are changed
+        private void DropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindSelectionData
+                (
+                    classesDropDown.SelectedIndex,
+                    difficultyDropDown.SelectedIndex
+                );
+        }
+
+        //// Log box code
+        // Focus logBox on hover to allow mouse scroll on hover
+        private void logBox_MouseHover(object sender, EventArgs e)
+        {
+            logBox.Focus();
+        }
+
+        // Update log box
+        private void BindLogData()
+        {
+            logBox.DataSource = null;
+            logBox.DataSource = riftsList;
+
+            // Scroll log box to bottom
+            logBox.TopIndex = logBox.Items.Count - 1;
+        }
+
+        // Alternate log box list item bg colors
+        private void logBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Boolean isSelected = ((e.State & DrawItemState.Selected) == DrawItemState.Selected);
+
+            if (e.Index > -1)
+            {
+                Color color = isSelected ?
+                    SystemColors.Highlight : e.Index % 2 == 0 ?
+                    Color.SkyBlue : Color.White;
+
+                SolidBrush bgBrush = new SolidBrush(color);
+                SolidBrush txtBrush = new SolidBrush(e.ForeColor);
+
+                e.Graphics.FillRectangle(bgBrush, e.Bounds);
+                e.Graphics.DrawString
+                    (
+                        logBox.GetItemText(logBox.Items[e.Index]), 
+                        e.Font,
+                        txtBrush,
+                        e.Bounds,
+                        StringFormat.GenericDefault
+                    );
+
+                bgBrush.Dispose();
+                txtBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+        //// End log box code
+
+        // Execute on exit
+        private void RiftTimer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Log rift times to file? Still not sure. Possibly in future version.
+        }
+    }
+}
