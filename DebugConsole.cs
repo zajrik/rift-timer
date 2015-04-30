e96640b303f443d4e93db22a454ca709e0882818
zajrik <zajrik@gmail.com>
2015-04-30 13:39:57 -0500

    Add DebugConsole for future debugging
    Because the Visual Studio output console likes to hide when I run debug
    builds and I'm tired of using MessageBox for my debug messages

---
+++ DebugConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace rift_timer
+{
+    class DebugConsole
+    {
+        public DebugConsole() { }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern Boolean AllocConsole();
+
+        public void Show()
+        {
+            AllocConsole();
+        }
+
+        public void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
